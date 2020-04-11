a311200a6074a3b262bebfcc79dbd8cb415240be
Bessarab123 <bessarab.kostik@mail.ru>
2020-04-11 16:59:59 +0300

    Обратотка фото и аудио

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import random
+from pprint import pprint
+
 import vk_api
 import json
+
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 
 def get_vk_session(user=True):
@@ -31,7 +36,33 @@ def get_vk_session(user=True):
     return vk_session
 
 
+def message():
+    pprint(media)
+    if media['attachments'][0]['type'] == 'audio_message':
+        id = media['attachments'][0]['audio_message']['id']
+        access_key = media['attachments'][0]['audio_message']['access_key']
+        vk.messages.send(user_id=user, attachment=f'audio_message{user}_{id}_{access_key}',
+                         random_id=random.randint(0, 2 ** 64))
+    elif False:
+        pass
+    else:
+        id = media['attachments'][0]['photo']['id']
+        access_key = media['attachments'][0]['photo']['access_key']
+
+        vk.messages.send(user_id=user,
+                         attachment=f'photo{user}_{id}_{access_key}',
+                         random_id=random.randint(0, 2 ** 64))
+
+
 if __name__ == '__main__':
-    vk_session = get_vk_session(True)
-    vk = vk_session.get_api()
+    vk_session = get_vk_session(False)
+    longpoll = VkBotLongPoll(vk_session, 193209431)
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+
+            vk = vk_session.get_api()
+            user = vk.users.get(user_ids=event.obj.message['from_id'])[0]['id']
+            print(user)
+            media = event.obj.message
+            # СДЕСЬ ФУНКЦИЯ КОТОРАЯ ОБРАБАЫТВЕТ СООБЩЕНИЯ И ГОТОВА ИХ ОТПРАЛЯТЬ
     # TODO тут что-то должно появиться
