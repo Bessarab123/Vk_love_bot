a8ed68e5aee3c09b1cd3e084dc36b6aa559cd7da
Svetlana <svetlana.telnova@icloud.com>
2020-04-12 17:37:01 +0300

    исправила ошибку

--- data/user.py
+++ data/user.py
@@ -6,10 +6,8 @@ from sqlalchemy_serializer import SerializerMixin
 
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
-
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    vk_id = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
