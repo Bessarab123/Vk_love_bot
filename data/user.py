7c373667a2f0daa050c580d2f8786872a370a993
Bessarab123 <bessarab.kostik@mail.ru>
2020-04-15 17:49:24 +0300

    База данных обнновлена

--- data/user.py
+++ data/user.py
@@ -6,8 +6,10 @@ from sqlalchemy_serializer import SerializerMixin
 
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
-    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True, primary_key=True)
+    vk_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True, primary_key=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     sex = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     city = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     scores = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    last_message = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
