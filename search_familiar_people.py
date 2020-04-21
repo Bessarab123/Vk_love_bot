be878445886a1ac963318e78aca2b0d84a8abc52
Svetlana <svetlana.telnova@icloud.com>
2020-04-21 23:09:59 +0300

    изменила параметры поиска

--- search_familiar_people.py
+++ search_familiar_people.py
@@ -1,10 +1,15 @@
 from main import *
 
 
-def search_for_familiar_people(user, age, city, sex, scores):
+def search_for_familiar_people(db_session, user, age, city, sex):
     session = db_session.create_session()
+    scores = user.scores
     age_dif = 1
     scores_dif = 100
+    if age is None:
+        age = user.age
+    if city is None:
+        city = user.city
     if age is None:
         age = 100
         age_dif = 100
@@ -13,7 +18,7 @@ def search_for_familiar_people(user, age, city, sex, scores):
         scores_dif = 10000
     friends = []
     for friend in session.query(User).filter(User.age >= age - age_dif, User.age <= age + age_dif,
-                                             User.sex == sex, User.scores >= scores - scores_dif,
+                                             User.scores >= scores - scores_dif,
                                              User.scores <= scores + scores_dif):
         if friend.vk_id != user.vk_id and (friend.city == city or city is None) and (friend.sex == sex or sex is None):
             friends.append(friend.vk_id)
